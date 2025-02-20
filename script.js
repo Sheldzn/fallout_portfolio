96bc0fc9a447ad75d3981fd1cd9c1025dc955070
Sheldzn <sjenkins.dev@gmail.com>
2025-02-20 16:49:50 +0200

    Finish content and styling

--- script.js
+++ script.js
@@ -41,3 +41,55 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  
+  function updateMaxHeight() {
+    const mainElement = document.querySelector('main'); // Get the single <main> element
+    const rightContents = document.querySelectorAll('.inner-right-content'); // Get all .inner-right-content elements
+  
+    if (mainElement) {
+      const mainHeight = mainElement.clientHeight; // Get main's height
+  
+      rightContents.forEach(content => {
+        content.style.maxHeight = `${mainHeight}px`; // Apply height to each .inner-right-content
+      });
+    }
+  }
+  
+  // Run on load and resize
+  window.addEventListener('load', updateMaxHeight);
+  window.addEventListener('resize', updateMaxHeight);
+  
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            let color = button.getAttribute("data-color");
+            document.documentElement.style.setProperty('--active-primary-color', `var(${color})`);
+            let colorClass = `color${color}`;
+            if (colorClass === 'color--neon-green') {
+                document.body.classList.add('neon-green');
+                document.body.classList.remove('neon-blue');
+                document.body.classList.remove('neon-amber');
+                document.body.classList.remove('neon-white');
+            } else if (colorClass === 'color--neon-blue') {
+                document.body.classList.add('neon-blue');
+                document.body.classList.remove('neon-green');
+                document.body.classList.remove('neon-amber');
+                document.body.classList.remove('neon-white');
+            } else if (colorClass === 'color--neon-amber') {
+                document.body.classList.add('neon-amber');
+                document.body.classList.remove('neon-green');
+                document.body.classList.remove('neon-blue');
+                document.body.classList.remove('neon-white');
+            } else if (colorClass === 'color--neon-white') {
+                document.body.classList.add('neon-white');
+                document.body.classList.remove('neon-green');
+                document.body.classList.remove('neon-blue');
+                document.body.classList.remove('neon-amber');
+            }
+        });
+    });
+});
